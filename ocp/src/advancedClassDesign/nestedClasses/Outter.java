7882983603792d6af9e6ae9576ef950b74e563f0
rafael.bernabeu <rafael.bernabeu@turismo.gov.br>
2018-03-08 16:08:09 -0300

    adding questions to be answered later.

---
+++ ocp/src/advancedClassDesign/nestedClasses/Outter.java
@@ -0,0 +1,19 @@
+package advancedClassDesign.nestedClasses;
+
+/**
+ * @author Rafael Bernabeu
+ * @date 08/03/18 - 16:03
+ *
+ * Questions that you have be able to answer:
+ *
+ * 1) Which variables declared in the encapsulating class or in the method, can an inner class access if the inner class is defined in a static method of encapsulating class?
+ * 2) Which variables of the encapsulating class can an inner class access, if the inner class is defined in an instance method of the encapsulating class?
+ *
+ *
+ *
+ */
+public class Outter {
+
+
+
+}
