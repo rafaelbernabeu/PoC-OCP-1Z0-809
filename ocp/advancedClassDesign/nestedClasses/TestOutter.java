3187fb789c16493f37d96ce3d2b02af3a9be03b3
Rafael <rbbernabeu@gmail.com>
2019-01-03 22:18:26 -0200

    update

--- ocp/advancedClassDesign/nestedClasses/TestOutter.java
+++ ocp/advancedClassDesign/nestedClasses/TestOutter.java
@@ -9,68 +9,56 @@ public class TestOutter {
     public static void main(String... args) {
 
 
-        assert new Outter().publicStaticMemberVariable.publicMSI == null;
-        assert new Outter().publicStaticMemberVariable.publicStaticMSI == null;
-        assert new Outter().publicStaticMemberVariable.privateMSI == null;
-        assert new Outter().publicStaticMemberVariable.privateStaticMSI == null;
+        assert Outter.publicStaticMemberVariable.publicMSI == null;
+        assert Outter.publicStaticMemberVariable.publicStaticMSI == null;
+//        assert new Outter().publicStaticMemberVariable.privateMSI == null;
+//        assert new Outter().publicStaticMemberVariable.privateStaticMSI == null;
 
-        assert new Outter().privateStaticMemberVariable.publicMSI == null;
-        assert new Outter().privateStaticMemberVariable.publicStaticMSI == null;
-        assert new Outter().privateStaticMemberVariable.privateMSI == null;
-        assert new Outter().privateStaticMemberVariable.privateStaticMSI == null;
+//        assert new Outter.PublicMemberStaticInner().privateMSI == null;
+//        assert new Outter.PublicMemberStaticInner().publicMSI == null;
+//        assert new Outter.PublicMemberStaticInner("").privateMSI == null;
+//        assert new Outter.PublicMemberStaticInner("").privateStaticMSI == null;
 
+        //instantiate static member class
+        assert new Outter.PublicMemberStaticInner("").publicMSI == null;
+
+        assert new Outter.PublicMemberStaticInner("").publicStaticMSI == null;
+        assert Outter.PublicMemberStaticInner.publicStaticMSI == null;
+
+//        assert new Outter().privateStaticMemberVariable.publicMSI == null;
+//        assert new Outter().privateStaticMemberVariable.publicStaticMSI == null;
+//        assert new Outter().privateStaticMemberVariable.privateMSI == null;
+//        assert new Outter().privateStaticMemberVariable.privateStaticMSI == null;
 
         assert new Outter().privateStaticMemberVariable == null;
+        assert Outter.privateStaticMemberVariable == null;
 
         assert new Outter().publicInstanceMemberVariable == null;
         assert new Outter().privateInstanceMemberVariable == null;
 
+//        assert new Outter().new PrivateMemberInner().privateMI == null;
+//        assert new Outter().new PrivateMemberInner().privateMI == null;
+//        assert new Outter().new PrivateMemberInner().publicMI == null;
 
-        //Private access tests
-        assert new Outter().new PrivateMemberInner().privateMI == null;
-        assert new Outter().new PrivateMemberInner().privateMI == null;
-        assert new Outter().new PrivateMemberInner().publicMI == null;
+//        assert new Outter.PrivateMemberStaticInner().privateMSI == null;
+//        assert new Outter.PrivateMemberStaticInner().publicMSI == null;
 
-        assert new Outter.PrivateMemberStaticInner().privateMSI == null;
-        assert new Outter.PrivateMemberStaticInner().publicMSI == null;
+//        assert new Outter.PrivateMemberStaticInner("").publicMSI == null;
+//        assert new Outter.PrivateMemberStaticInner("").publicStaticMSI == null;
+//        assert new Outter.PrivateMemberStaticInner("").privateMSI == null;
+//        assert new Outter.PrivateMemberStaticInner("").privateStaticMSI == null;
+//        assert new Outter.PrivateMemberStaticInner().publicMSI == null;
+//        assert new Outter.PrivateMemberStaticInner().publicStaticMSI == null;
+//        assert new Outter.PrivateMemberStaticInner().privateMSI == null;
+//        assert new Outter.PrivateMemberStaticInner().privateStaticMSI == null;
 
-        assert new Outter.PrivateMemberStaticInner("").publicMSI == null;
-        assert new Outter.PrivateMemberStaticInner("").publicStaticMSI == null;
-        assert new Outter.PrivateMemberStaticInner("").privateMSI == null;
-        assert new Outter.PrivateMemberStaticInner("").privateStaticMSI == null;
-        assert new Outter.PrivateMemberStaticInner().publicMSI == null;
-        assert new Outter.PrivateMemberStaticInner().publicStaticMSI == null;
-        assert new Outter.PrivateMemberStaticInner().privateMSI == null;
-        assert new Outter.PrivateMemberStaticInner().privateStaticMSI == null;
+//        assert new Outter().new PublicMemberInnerClass().privateMI == null;              //private no-args constructor
+//        assert new Outter().new PublicMemberInnerClass().publicMI == null;              //private no-args constructor
+//        assert new Outter().new PublicMemberInnerClass("").privateMI == null;
 
-
-        //Public access tests
-        assert new Outter().new PublicMemberInnerClass().privateMI == null;              //private no-args constructor
-        assert new Outter().new PublicMemberInnerClass().publicMI == null;              //private no-args constructor
-        assert new Outter().new PublicMemberInnerClass("").privateMI == null;
+        //instantiate member class
         assert new Outter().new PublicMemberInnerClass("").publicMI == null;
 
-        assert new Outter.PublicMemberStaticInner().privateMSI == null;
-        assert new Outter.PublicMemberStaticInner().publicMSI == null;
-        assert new Outter.PublicMemberStaticInner("").privateMSI == null;
-        assert new Outter.PublicMemberStaticInner("").privateStaticMSI == null;
-        assert new Outter.PublicMemberStaticInner("").publicMSI == null;
-        assert new Outter.PublicMemberStaticInner("").publicStaticMSI == null;
-
-
-        assert new Outter.PublicMemberStaticInner("").privateMSI == null;
-        assert new Outter.PublicMemberStaticInner("").publicMSI == null;
-        assert new Outter.PublicMemberStaticInner("").privateMSI == null;
-        assert new Outter.PublicMemberStaticInner("").publicMSI == null;
-
-        assert new Outter.PublicMemberStaticInner("").publicMSI == null;
-        assert new Outter.PublicMemberStaticInner("").publicStaticMSI == null;
-        assert new Outter.PublicMemberStaticInner("").privateMSI == null;
-        assert new Outter.PublicMemberStaticInner("").privateStaticMSI == null;
-        assert new Outter.PublicMemberStaticInner().publicMSI == null;
-        assert new Outter.PublicMemberStaticInner().publicStaticMSI == null;
-        assert new Outter.PublicMemberStaticInner().privateMSI == null;
-        assert new Outter.PublicMemberStaticInner().privateStaticMSI == null;
 
     }
 
---
+++ ocp/concurrency/ThreadExecutors.java
@@ -0,0 +1,85 @@
+package concurrency;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadExecutors {
+
+    //totally subjective to the task and the system.
+    private static final int cores = Runtime.getRuntime().availableProcessors();
+
+    private static ExecutorService singleThread = Executors.newSingleThreadExecutor();
+    private static ScheduledExecutorService singleThreadScheduled = Executors.newSingleThreadScheduledExecutor();
+
+    //Creates new threads as needed, reuse previously created threads when they are available.
+    private static ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+
+    //If number of tasks is greater than our number of threads, tasks will wait available threads.
+    private static ExecutorService fixedThreadPool = Executors.newFixedThreadPool(cores);
+    private static ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(cores);
+
+    private class Example {
+
+        private Callable<String> callable = () -> {
+            for (int i = 0; i < 1000; i++) {
+                System.out.println(i);
+            }
+            return "i";
+        };
+        private Runnable runnable = () -> {
+            for (int i = 0; i < 1000; i++) {
+                System.out.println(i);
+            }
+        };
+
+        private void tests() {
+
+            int delay = 10;
+            int initialDelay = 0;
+
+            fixedThreadPool.submit(callable);
+
+            //Executes after the giver delay
+            scheduledThreadPool.schedule(callable, delay, TimeUnit.SECONDS);
+
+            //Delay between the end of a thread and start of the next
+            scheduledThreadPool.scheduleWithFixedDelay(runnable, initialDelay, delay, TimeUnit.SECONDS);
+
+            //Delay between the start of a thread regardless of the previous task have finished
+            scheduledThreadPool.scheduleAtFixedRate(runnable, initialDelay, delay, TimeUnit.SECONDS);
+
+            try {
+                Set<Callable<String>> tasks = Collections.singleton(callable);
+
+                //runs synchronously, wait until the results are available to return control to enclosing program
+                List<Future<String>> futures = fixedThreadPool.invokeAll(tasks);
+
+                //runs synchronously, return one of the tasks and cancel any unfinished tasks.
+                String result = fixedThreadPool.invokeAny(tasks);
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+
+
+
+
+        }
+
+
+    }
+
+
+    public static void main(String[] args) {
+        new ThreadExecutors().new Example().tests();
+    }
+
+
+}
