39f31e7df50fbd50117d7e9509913ea20d90e09a
Rafael <rbbernabeu@gmail.com>
2019-01-05 23:50:13 -0200

    fork join

---
+++ src/main/java/util/SingletonUtil.java
@@ -0,0 +1,43 @@
+package util;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SingletonUtil {
+
+    private static final Map<Class, Object> singletonInstances = new HashMap<>();
+
+    public static <T> T createInstance(Class<T> requestedInstance) {
+        return createInstance(requestedInstance, null);
+    }
+
+    public static <T, E> T createInstance(Class<T> requestedInstance, Class<E>[] parameterTypes, Object... objArgs) {
+        try {
+            return (T) putInstance(requestedInstance.getDeclaredConstructor(parameterTypes).newInstance(objArgs));
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static <T> T putInstance(T instance) {
+        return SingletonUtil.putInstance(instance, Boolean.FALSE);
+    }
+
+    public static <T> T putInstance(T instance, Boolean override) {
+        if (override || !singletonInstances.containsKey(instance.getClass())) {
+            synchronized (SingletonUtil.class) {
+                if (!singletonInstances.containsKey(instance.getClass()) || override) {
+                    singletonInstances.put(instance.getClass(), instance);
+                }
+            }
+        }
+        return instance;
+    }
+
+    public static <T> T getInstance(Class<T> requestedInstance) {
+        return (T) singletonInstances.get(requestedInstance);
+    }
+
+}
